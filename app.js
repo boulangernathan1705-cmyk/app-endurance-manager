0d15568420ffce50246d950fb1e78c947bb0920b
boulangernathan1705-cmyk <boulanger.nathan1705@gmail.com>
2026-09-16 17:48:35 +0200

    chore: load new groups interface modules

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import {state} from './front/app/core.mjs';
 import {installCrewDepartureOpenState} from './front/crew-departure-open-state.mjs?v=1';
-import './front/app/actions.mjs?v=25-organizations';
-import './front/app/organizations-view.mjs?v=1';
-import './front/app/organization-entry-bridge.mjs?v=1';
+import './front/app/actions.mjs?v=26-audiences';
+import './front/app/organizations-view.mjs?v=2-groups';
+import './front/app/organization-entry-bridge.mjs?v=2-audiences';
 
 installCrewDepartureOpenState(state);
