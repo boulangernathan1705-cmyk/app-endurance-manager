04a879d962cfb42ff010b1d248c3079b4d84c211
boulangernathan1705-cmyk <boulanger.nathan1705@gmail.com>
2026-09-23 07:51:13 +0200

    Bump application community cache version

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import {state} from './front/app/core.mjs';
 import {installCrewDepartureOpenState} from './front/crew-departure-open-state.mjs?v=1';
-import './front/app/actions.mjs?v=30-community-context';
+import './front/app/actions.mjs?v=31-community-branding';
 import './front/app/paddock-network.mjs?v=3-community-context';
 
 installCrewDepartureOpenState(state);
