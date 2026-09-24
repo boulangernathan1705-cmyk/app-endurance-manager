4b4437285a9e86066d74465cc15b332842f5a336
boulangernathan1705-cmyk <boulanger.nathan1705@gmail.com>
2026-09-24 06:04:16 +0200

    Unify Tondeuz module cache version

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {state} from './front/app/core.mjs?v=12-site-tool';
+import {state} from './front/app/core.mjs?v=13-tondeuz-tool';
 import {installCrewDepartureOpenState} from './front/crew-departure-open-state.mjs?v=1';
 import './front/app/actions.mjs?v=43-site-tool';
 import './front/app/paddock-network.mjs?v=10-site-tool';
